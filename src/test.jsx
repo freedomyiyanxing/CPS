aed336a36f561a9a7f2ff006a48e72e05c62375a
freedom.yi <851989962freedom@gmail.com>
2019-09-05 17:14:43 +0800

    测试 git的换行符1

--- src/test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Test = () => {
-  console.log('222');
-  return <div>Test</div>;
-};
-
-export default Test;
---
+++ src/test1.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Test1 = () => <div>hha</div>;
+
+export default Test1;
